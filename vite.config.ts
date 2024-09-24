bf667a1fa2e90075b7fb2c37cccfe7a0ea1b0ee5
Nick Local <thenick112@gmail.com>
2024-09-24 12:59:51 +0000

    trying to get it to build

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,18 @@ import mkcert from 'vite-plugin-mkcert';
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
 
 export default defineConfig({
+  worker: {
+      format: "es",
+
+},
 	server: {
 		https: true,
 		proxy: {}
 	},
+  build: {
+    target: "es2022"
+
+  },
 
 	plugins: [sveltekit(), mkcert(), crossOriginIsolation()]
 });
